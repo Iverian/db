5e596d77424c8c42ecdcdbfb58b9af9674338c30
Iverian <41ways1ucky@gmail.com>
2016-11-26 20:45:40 +0300

    db_admin mod

--- db_admin/Debug/debug/moc_MainWindow.cpp
+++ db_admin/Debug/debug/moc_MainWindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[19];
-    char stringdata0[492];
+    QByteArrayData data[21];
+    char stringdata0[547];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,37 +34,41 @@ QT_MOC_LITERAL(1, 11, 28), // "on_actNewOperation_triggered"
 QT_MOC_LITERAL(2, 40, 0), // ""
 QT_MOC_LITERAL(3, 41, 24), // "on_actNewOrder_triggered"
 QT_MOC_LITERAL(4, 66, 30), // "on_actNewStaffMember_triggered"
-QT_MOC_LITERAL(5, 97, 26), // "on_operNames_doubleClicked"
-QT_MOC_LITERAL(6, 124, 5), // "index"
-QT_MOC_LITERAL(7, 130, 27), // "on_orderNames_doubleClicked"
-QT_MOC_LITERAL(8, 158, 39), // "on_operNames_customContextMen..."
-QT_MOC_LITERAL(9, 198, 3), // "pos"
-QT_MOC_LITERAL(10, 202, 40), // "on_orderNames_customContextMe..."
-QT_MOC_LITERAL(11, 243, 26), // "on_staffView_doubleClicked"
-QT_MOC_LITERAL(12, 270, 39), // "on_staffView_customContextMen..."
+QT_MOC_LITERAL(5, 97, 39), // "on_operNames_customContextMen..."
+QT_MOC_LITERAL(6, 137, 3), // "pos"
+QT_MOC_LITERAL(7, 141, 40), // "on_orderNames_customContextMe..."
+QT_MOC_LITERAL(8, 182, 39), // "on_staffView_customContextMen..."
+QT_MOC_LITERAL(9, 222, 26), // "on_operNames_doubleClicked"
+QT_MOC_LITERAL(10, 249, 5), // "index"
+QT_MOC_LITERAL(11, 255, 27), // "on_orderNames_doubleClicked"
+QT_MOC_LITERAL(12, 283, 26), // "on_staffView_doubleClicked"
 QT_MOC_LITERAL(13, 310, 29), // "on_actEditOperation_triggered"
 QT_MOC_LITERAL(14, 340, 25), // "on_actEditOrder_triggered"
 QT_MOC_LITERAL(15, 366, 31), // "on_actEditStaffMember_triggered"
 QT_MOC_LITERAL(16, 398, 31), // "on_actDeleteOperation_triggered"
 QT_MOC_LITERAL(17, 430, 27), // "on_actDeleteOrder_triggered"
-QT_MOC_LITERAL(18, 458, 33) // "on_actDeleteStaffMember_trigg..."
+QT_MOC_LITERAL(18, 458, 33), // "on_actDeleteStaffMember_trigg..."
+QT_MOC_LITERAL(19, 492, 26), // "operNames_selectionChanged"
+QT_MOC_LITERAL(20, 519, 27) // "orderNames_selectionChanged"
 
     },
     "MainWindow\0on_actNewOperation_triggered\0"
     "\0on_actNewOrder_triggered\0"
     "on_actNewStaffMember_triggered\0"
-    "on_operNames_doubleClicked\0index\0"
-    "on_orderNames_doubleClicked\0"
     "on_operNames_customContextMenuRequested\0"
     "pos\0on_orderNames_customContextMenuRequested\0"
-    "on_staffView_doubleClicked\0"
     "on_staffView_customContextMenuRequested\0"
+    "on_operNames_doubleClicked\0index\0"
+    "on_orderNames_doubleClicked\0"
+    "on_staffView_doubleClicked\0"
     "on_actEditOperation_triggered\0"
     "on_actEditOrder_triggered\0"
     "on_actEditStaffMember_triggered\0"
     "on_actDeleteOperation_triggered\0"
     "on_actDeleteOrder_triggered\0"
-    "on_actDeleteStaffMember_triggered"
+    "on_actDeleteStaffMember_triggered\0"
+    "operNames_selectionChanged\0"
+    "orderNames_selectionChanged"
 };
 #undef QT_MOC_LITERAL
 
@@ -74,7 +78,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      15,   14, // methods
+      17,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -82,32 +86,36 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   89,    2, 0x08 /* Private */,
-       3,    0,   90,    2, 0x08 /* Private */,
-       4,    0,   91,    2, 0x08 /* Private */,
-       5,    1,   92,    2, 0x08 /* Private */,
-       7,    1,   95,    2, 0x08 /* Private */,
-       8,    1,   98,    2, 0x08 /* Private */,
-      10,    1,  101,    2, 0x08 /* Private */,
-      11,    1,  104,    2, 0x08 /* Private */,
-      12,    1,  107,    2, 0x08 /* Private */,
-      13,    0,  110,    2, 0x08 /* Private */,
-      14,    0,  111,    2, 0x08 /* Private */,
-      15,    0,  112,    2, 0x08 /* Private */,
-      16,    0,  113,    2, 0x08 /* Private */,
-      17,    0,  114,    2, 0x08 /* Private */,
-      18,    0,  115,    2, 0x08 /* Private */,
+       1,    0,   99,    2, 0x08 /* Private */,
+       3,    0,  100,    2, 0x08 /* Private */,
+       4,    0,  101,    2, 0x08 /* Private */,
+       5,    1,  102,    2, 0x08 /* Private */,
+       7,    1,  105,    2, 0x08 /* Private */,
+       8,    1,  108,    2, 0x08 /* Private */,
+       9,    1,  111,    2, 0x08 /* Private */,
+      11,    1,  114,    2, 0x08 /* Private */,
+      12,    1,  117,    2, 0x08 /* Private */,
+      13,    0,  120,    2, 0x08 /* Private */,
+      14,    0,  121,    2, 0x08 /* Private */,
+      15,    0,  122,    2, 0x08 /* Private */,
+      16,    0,  123,    2, 0x08 /* Private */,
+      17,    0,  124,    2, 0x08 /* Private */,
+      18,    0,  125,    2, 0x08 /* Private */,
+      19,    0,  126,    2, 0x08 /* Private */,
+      20,    0,  127,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QModelIndex,    6,
-    QMetaType::Void, QMetaType::QModelIndex,    6,
-    QMetaType::Void, QMetaType::QPoint,    9,
-    QMetaType::Void, QMetaType::QPoint,    9,
-    QMetaType::Void, QMetaType::QModelIndex,    6,
-    QMetaType::Void, QMetaType::QPoint,    9,
+    QMetaType::Void, QMetaType::QPoint,    6,
+    QMetaType::Void, QMetaType::QPoint,    6,
+    QMetaType::Void, QMetaType::QPoint,    6,
+    QMetaType::Void, QMetaType::QModelIndex,   10,
+    QMetaType::Void, QMetaType::QModelIndex,   10,
+    QMetaType::Void, QMetaType::QModelIndex,   10,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -127,18 +135,20 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->on_actNewOperation_triggered(); break;
         case 1: _t->on_actNewOrder_triggered(); break;
         case 2: _t->on_actNewStaffMember_triggered(); break;
-        case 3: _t->on_operNames_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 4: _t->on_orderNames_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 5: _t->on_operNames_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 6: _t->on_orderNames_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 7: _t->on_staffView_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 8: _t->on_staffView_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 3: _t->on_operNames_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 4: _t->on_orderNames_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 5: _t->on_staffView_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 6: _t->on_operNames_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 7: _t->on_orderNames_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 8: _t->on_staffView_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
         case 9: _t->on_actEditOperation_triggered(); break;
         case 10: _t->on_actEditOrder_triggered(); break;
         case 11: _t->on_actEditStaffMember_triggered(); break;
         case 12: _t->on_actDeleteOperation_triggered(); break;
         case 13: _t->on_actDeleteOrder_triggered(); break;
         case 14: _t->on_actDeleteStaffMember_triggered(); break;
+        case 15: _t->operNames_selectionChanged(); break;
+        case 16: _t->orderNames_selectionChanged(); break;
         default: ;
         }
     }
@@ -169,13 +179,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 15)
+        if (_id < 17)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 15;
+        _id -= 17;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 15)
+        if (_id < 17)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 15;
+        _id -= 17;
     }
     return _id;
 }
